7e7ed4392b2a0115f4d5ecbfe23959e687a7d0d3
Emiliano R Gillini <emigillini@hotmail.com>
2023-03-01 09:07:52 -0300

    clase3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import saludar from './prueba';
 
+let styles = {
+  borderColor: "black",
+  marginLeft: 500
+};
+
+const letra =() =>{
+  alert("muy bien")
+}
+
 function App() {
   saludar()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={styles}>
          "EMILIANO GILLINI"
         </p>
+        <button id='bot1' onClick={letra}>soy </button>
         
         <h2>VIVA</h2>
         <a
